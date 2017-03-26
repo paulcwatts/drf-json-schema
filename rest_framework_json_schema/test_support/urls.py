f6d91256a7c6b3f33308a276ebebdbb45ad91559
Paul Watts <paulcwatts@gmail.com>
2017-03-26 09:10:50 -0700

    Better exception case
    Make it more clear what needs to be done when the renderer can’t find a
    serializer in the data.

--- rest_framework_json_schema/test_support/urls.py
+++ rest_framework_json_schema/test_support/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url, include
 from rest_framework import routers
-from .views import ArtistViewSet, AlbumViewSet, TrackViewSet, PaginateViewSet
+from .views import (ArtistViewSet, AlbumViewSet, TrackViewSet, PaginateViewSet, NonJSONPaginateViewSet)
 
 
 router = routers.DefaultRouter()
@@ -8,6 +8,7 @@ router.register(r'artist', ArtistViewSet, 'artist')
 router.register(r'album', AlbumViewSet, 'album')
 router.register(r'track', TrackViewSet, 'track')
 router.register(r'paged', PaginateViewSet, 'page')
+router.register(r'paged-nonjson', NonJSONPaginateViewSet, 'page-nonjson')
 
 urlpatterns = [
     url(r'^api/', include(router.urls))
